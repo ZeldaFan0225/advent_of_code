d95910f3c9c2f29cecea2e8319a2165cbcc72a47
ZeldaFan0225 <robin.butsch@gmail.com>
2022-12-02 11:10:34 +0100

    day 2

---
+++ 2022/javascript/day_2/day_2_1.js
@@ -0,0 +1,31 @@
+const {readFileSync} = require("fs")
+
+const input = readFileSync("./input.txt").toString("utf-8").replace(/\r/g, "")
+
+// A => Rock
+// B => Paper
+// C => Scissors
+
+// X => Rock => 1
+// Y => Paper => 2
+// Z => Scissors => 3
+
+const games = input.split("\n")
+
+const results = games.map(g => {
+    const [opponent, own] = g.split(" ")
+    let score = own === "X" ? 1 : own === "Y" ? 2 : 3
+    switch(opponent + own) {
+        case "CX":
+        case "BZ":
+        case "AY": score += 6; break;
+        case "AX":
+        case "BY":
+        case "CZ": score += 3; break;
+    }
+    return score
+})
+
+const total = results.reduce((a, b) => a + b)
+
+console.log(total)
---
+++ 2022/javascript/day_2/day_2_2.js
@@ -0,0 +1,43 @@
+const {readFileSync} = require("fs")
+
+const input = readFileSync("./input.txt").toString("utf-8").replace(/\r/g, "")
+
+// A => Rock
+// B => Paper
+// C => Scissors
+
+// Rock => 1
+// Paper => 2
+// Scissors => 3
+
+// X => Lose
+// Y => Draw
+// Z => Win
+
+const games = input.split("\n")
+
+const results = games.map(g => {
+    const [opponent, outcome] = g.split(" ")
+    let score = 0
+    switch(outcome) {
+        case "X": {
+            score += opponent === "A" ? 3 : opponent === "B" ? 1 : 2;
+            break;
+        }
+        case "Y": {
+            score += 3
+            score += opponent === "A" ? 1 : opponent === "B" ? 2 : 3;
+            break;
+        }
+        case "Z": {
+            score += 6
+            score += opponent === "A" ? 2 : opponent === "B" ? 3 : 1;
+            break;
+        }
+    }
+    return score
+})
+
+const total = results.reduce((a, b) => a + b)
+
+console.log(total)
