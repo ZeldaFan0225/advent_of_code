0c5bc58f4347c0ad63113fa87ef234f6e586fbcd
ZeldaFan0225 <robin.butsch@gmail.com>
2022-12-03 10:38:12 +0100

    day 3

---
+++ 2022/javascript/day_3/day_3_1.js
@@ -0,0 +1,20 @@
+const {readFileSync} = require("fs")
+
+const input = readFileSync("./input.txt").toString("utf-8").replace(/\r/g, "")
+const rucksacks = input.split("\n")
+
+const doubled_item_priorities = rucksacks.map(r => {
+    const [comp1, comp2] = [r.slice(0, r.length/2), r.slice(r.length/2)]
+    const [comp1_arr, comp2_arr] = [comp1.split(""), comp2.split("")]
+    const same = comp1_arr.find(l => comp2_arr.includes(l))
+    
+    const priority = same.charCodeAt(0) - (isUpperCase(same) ? 38 : 96)
+    return priority
+})
+
+function isUpperCase(letter) {
+    return letter.toUpperCase() === letter
+}
+
+const result = doubled_item_priorities.reduce((a, b) => a+b)
+console.log(result)
---
+++ 2022/javascript/day_3/day_3_2.js
@@ -0,0 +1,22 @@
+const {readFileSync} = require("fs")
+
+const input = readFileSync("./input.txt").toString("utf-8").replace(/\r/g, "").split("\n")
+const teams = Array.from({length: input.length/3}, () => input.splice(0, 3))
+
+const badge_priorities = teams.map(t => {
+    const [member_1_arr, member_2_arr, member_3_arr] = t.map(r => r.split(""))
+    const same = member_1_arr.find(l => member_2_arr.includes(l) && member_3_arr.includes(l))
+    const priority = getPriority(same)
+    return priority
+})
+
+function getPriority(letter) {
+    return letter.charCodeAt(0) - (isUpperCase(letter) ? 38 : 96)
+}
+
+function isUpperCase(letter) {
+    return letter.toUpperCase() === letter
+}
+
+const result = badge_priorities.reduce((a, b) => a + b)
+console.log(result)
