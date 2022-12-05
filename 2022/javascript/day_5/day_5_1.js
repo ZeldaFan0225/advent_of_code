1ddf15b90bca29ce5ec0faaf58e83a7751efc195
ZeldaFan0225 <robin.butsch@gmail.com>
2022-12-05 16:41:45 +0100

    why wont it work

--- 2022/javascript/day_5/day_5_1.js
+++ 2022/javascript/day_5/day_5_1.js
@@ -1,18 +1,35 @@
 const {readFileSync} = require("fs")
 
 const input = readFileSync("./input.txt", "utf-8").replace(/\r/g, "")
-
 const [cargo_full, moves_full] = input.split("\n 1   2   3   4   5   6   7   8   9 \n\n")
 
 const moves = moves_full.split("\n").map(d => {
     const nums = d.match(/(\d+)/g)
     return {
         move: parseInt(nums[0]),
-        from: parseInt(nums[1]),
-        to: parseInt(nums[2])
+        from: parseInt(nums[1])-1,
+        to: parseInt(nums[2])-1
     }
 })
 
-// make cargo 2 dimensional array
+const cargo_rows = cargo_full.split("\n")
+const cargo = Array.from({length: 9}, () => ([]))
+cargo_rows.forEach((row) => {
+    let i = 0
+    while(true) {
+        const container = row.slice(i * 4, (i+1) * 4)
+        if(!container.length) break;
+        if(container.trim()) {
+            cargo[i].unshift(container.trim().replace(/\[|\]/g, ""))
+        }
+        ++i;
+    }
+})
 
-console.log(cargo)
+
+for(let move of moves) {
+    const removed = cargo[move.from].splice(-move.move)
+    cargo[move.to].push(...removed)
+}
+
+console.log(cargo.map(r => r.at(-1)).join(""))
