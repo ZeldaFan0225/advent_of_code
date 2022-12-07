49011e32fbda2d613e42b6283e3080d0a609384d
ZeldaFan0225 <robin.butsch@gmail.com>
2022-12-07 15:01:55 +0100

    day 7-1

--- 2022/javascript/day_7/day_7_1.js
+++ 2022/javascript/day_7/day_7_1.js
@@ -1,30 +1,53 @@
 const {readFileSync} = require("fs")
 
-const input = readFileSync("./input.txt", "utf-8")
+const input = readFileSync("./input.txt", "utf-8").replace(/\r/g, "")
 const lines = input.split("\n")
 
 let current_dir = ""
-let file_system = {}
+let file_system = {dirs: {}, files: []}
 
 for(let line of lines) {
     if(line.startsWith("$")) {
-        if(line.startsWith("& cd")) {
-            // cd
+        if(line.startsWith("$ cd")) {
             let path = line.replace("$ cd ", "")
             if(path === "..") {
-                current_dir = current_dir.split("/").splice(0, -1)
+                current_dir = current_dir.split("/").slice(0, -1).join("/").trim()
+                current_dir = (current_dir === "/" ? "" : current_dir)
             } else {
                 current_dir += `/${path}`
             }
         }
     } else if(line.startsWith("dir")) {
         let dir = line.replace("dir ", "")
-        //add dir to CURRENT dir
-        // dir
+        let temp = file_system
+        current_dir.split("/").slice(1).forEach(d => temp = temp.dirs[d])
+        temp.dirs[dir] = {dirs: {}, files: []}
     } else {
         let [size, name] = line.split(" ")
-        // file
-        //add file to CURRENT dir
-
+        let temp = file_system
+        current_dir.split("/").slice(1).forEach(d => temp = temp.dirs[d])
+        temp.files.push({name,size: parseInt(size)})
     }
 }
+
+function getFilesSize(files) {
+    if(!files.length) return 0
+    return files.map(f => f.size).reduce((a,b) => a+b)
+}
+
+let large_dirs = []
+
+function getFileSizePlusInnerDirectories(layer) {
+    const files = getFilesSize(layer.files)
+    let dir_file_sizes = 0
+    if(Object.keys(layer.dirs).length) {
+        dir_file_sizes = Object.values(layer.dirs).map(d => getFileSizePlusInnerDirectories(d)).reduce((a,b) => a+b)
+    }
+    if(files + dir_file_sizes <= 100000) large_dirs.push(files + dir_file_sizes)
+    return files + dir_file_sizes
+}
+
+file_system.files = []
+getFileSizePlusInnerDirectories(file_system)
+
+console.log(large_dirs.reduce((a,b) => a+b))
---
+++ 2022/javascript/day_7/day_7_2.js
@@ -0,0 +1,71 @@
+const {readFileSync} = require("fs")
+
+const input = readFileSync("./input.txt", "utf-8").replace(/\r/g, "")
+const lines = input.split("\n")
+
+let current_dir = ""
+let file_system = {dirs: {}, files: []}
+
+for(let line of lines) {
+    if(line.startsWith("$")) {
+        if(line.startsWith("$ cd")) {
+            let path = line.replace("$ cd ", "")
+            if(path === "..") {
+                current_dir = current_dir.split("/").slice(0, -1).join("/").trim()
+                current_dir = (current_dir === "/" ? "" : current_dir)
+            } else {
+                current_dir += `/${path}`
+            }
+        }
+    } else if(line.startsWith("dir")) {
+        let dir = line.replace("dir ", "")
+        let temp = file_system
+        current_dir.split("/").slice(1).forEach(d => temp = temp.dirs[d])
+        temp.dirs[dir] = {dirs: {}, files: []}
+    } else {
+        let [size, name] = line.split(" ")
+        let temp = file_system
+        current_dir.split("/").slice(1).forEach(d => temp = temp.dirs[d])
+        temp.files.push({name,size: parseInt(size)})
+    }
+}
+
+function getFilesSize(files) {
+    if(!files.length) return 0
+    return files.map(f => f.size).reduce((a,b) => a+b)
+}
+
+function getFileSizePlusInnerDirectories(layer) {
+    const files = getFilesSize(layer.files)
+    let dir_file_sizes = 0
+    if(Object.keys(layer.dirs).length) {
+        dir_file_sizes = Object.values(layer.dirs).map(d => getFileSizePlusInnerDirectories(d)).reduce((a,b) => a+b)
+    }
+    return files + dir_file_sizes
+}
+
+let dir_and_size = []
+
+function mapDirSizes(layer, dir) {
+    const files = getFilesSize(layer.files)
+    let dir_file_sizes = 0
+    if(Object.keys(layer.dirs).length) {
+        dir_file_sizes = Object.entries(layer.dirs).map(([k, v]) => mapDirSizes(v, dir+"/"+k)).reduce((a,b) => a+b)
+    }
+    dir_and_size.push({
+        path: dir,
+        size: files + dir_file_sizes
+    })
+    return files + dir_file_sizes
+}
+
+const total_used = getFileSizePlusInnerDirectories(file_system)
+const need_to_delete = 70000000 - total_used
+mapDirSizes(file_system, "")
+console.log(need_to_delete)
+
+let sorted = dir_and_size.map(p => ({path: p.path, diff: p.size-need_to_delete, size: p.size})).filter(n => n.diff >= 0).sort((a,b) => a.diff-b.diff)
+
+// only working for test input
+console.log(sorted)
+console.log(sorted[0].size)
