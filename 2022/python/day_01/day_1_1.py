8f89857b139ae49819f50c668ebf2d75b11a739c
ZeldaFan0225 <78901316+ZeldaFan0225@users.noreply.github.com>
2023-07-07 13:17:00 +0200

    day 1-1

--- 2022/python/day_01/day_1_1.py
+++ 2022/python/day_01/day_1_1.py
@@ -1,3 +1,9 @@
-inp = open("input.txt", "r").readlines()
+import functools
+inp = open("input.txt", "r").read().replace(r"\r\n", "").split("\n\n")
 
-print(inp)
+values = list(map((lambda elf: list(map((lambda food: int(food)), elf.split("\n")))), inp))
+
+total_calories = list(map(lambda elf: functools.reduce(lambda a, b: a+b, elf), values))
+total_calories.sort(reverse=True)
+
+print(f'The backpack with the most calories has {total_calories[0]} calories inside')
