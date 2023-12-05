51844ff6ca28e4c4bad2899e6e57bc3bacb3d0c9
ZeldaFan0225 <78901316+ZeldaFan0225@users.noreply.github.com>
2023-12-05 17:08:21 +0100

    day 5-1 finished

--- 2023/javascript/day_05/day_5_1.js
+++ 2023/javascript/day_05/day_5_1.js
@@ -4,42 +4,38 @@ const input = readFileSync("./input.txt", "utf-8").replace(/\r/g, "")
 const lines = input.split("\n\n")
 
 const seeds_string = lines.splice(0, 1)[0]
-const maps = {}
+const categories = []
 const seeds = seeds_string.split(" ").slice(1).map(n => parseInt(n))
 
-for(let map of lines) {
-    const maplines = map.split("\n")
-    const mapname = maplines.splice(0, 1)[0].replace(" map:", "")
-    maps[mapname] = []
+for(let category of lines) {
+    const category_lines = category.split("\n").slice(1)
+    categories.push([])
 
-    maplines.forEach(m => {
-        const [destination, source, range] = m.split(" ").map(n => parseInt(n))
-        maps[mapname].push({
+    category_lines.forEach(c => {
+        const [destination, source, range] = c.split(" ").map(n => parseInt(n))
+        categories.at(-1).push({
             source_start: source,
-            source_end: source + range - 1,
-            destination,
-            range,
+            source_end: source + (range - 1),
+            //destination,
+            //range,
             diff: destination - source
         })
     })
 }
 
-console.log(maps)
-
-for(let processing of Object.values(maps)) {
-    for(let i = 0; i < seeds.length; i++) {
-        const seed = seeds[i]
-        for(let map of processing) {
-            if (seed >= map.source_start && seed <= map.source_end) {
-                seeds[i] = seed + map.diff
+for(let category of categories) {
+    for(let i in seeds) {
+        for(let map of category) {
+            if(seeds[i] >= map.source_start && seeds[i] <= map.source_end) {
+                seeds[i] += map.diff
                 break;
             }
         }
     }
+    console.log(seeds)
 }
 
-
-// Too high:1136789458
+seeds.sort((a, b) => a - b)
 
 console.log("The locations are: ", seeds.join(", "))
-console.log("The lowest location is: ", seeds.sort()[0])
+console.log("The lowest location is: ", seeds[0])
