98fc6da920fd12c743d745399c18c45512d3efde
ZeldaFan0225 <robin.butsch@gmail.com>
2023-12-06 08:42:39 +0100

    complete day 6

---
+++ 2023/javascript/day_06/day_6_1.js
@@ -0,0 +1,43 @@
+const {readFileSync} = require("fs")
+const BENCHMARK_START = process.hrtime.bigint()
+
+const input = readFileSync("./input.txt", "utf-8").replace(/\r/g, "")
+const lines = input.split("\n")
+const number_regex = /\d+/g
+
+const times = []
+const distances = []
+const tolerances = []
+
+let match;
+while((match = number_regex.exec(lines[0])) != null) {
+    times.push(parseInt(match[0]))
+}
+while((match = number_regex.exec(lines[1])) != null) {
+    distances.push(parseInt(match[0]))
+}
+
+for(let i in times) {
+    const ends = getEnds(times[i], distances[i])
+    const diff = ends.upper - ends.lower + 1
+
+    tolerances.push(diff)
+}
+
+function getEnds(time, distance) {
+    const fraction = time / 2;
+    const t1 = fraction - Math.sqrt(fraction**2 - (distance + 1))
+    const t2 = fraction + Math.sqrt(fraction**2 - (distance + 1))
+
+    return {
+        lower: Math.ceil(t1),
+        upper: Math.floor(t2)
+    }
+}
+
+const ways_count = tolerances.reduce((a, b) => a * b)
+
+console.log(`The amount of possible ways to win are: ${ways_count}`)
+
+const BENCHMARK_END = process.hrtime.bigint()
+console.log(`Execution time: ${(BENCHMARK_END - BENCHMARK_START) / 1000000n}ms`)
---
+++ 2023/javascript/day_06/day_6_2.js
@@ -0,0 +1,27 @@
+const {readFileSync} = require("fs")
+const BENCHMARK_START = process.hrtime.bigint()
+
+const input = readFileSync("./input.txt", "utf-8").replace(/\r/g, "")
+const lines = input.split("\n")
+
+const time = parseInt(/\d+/g.exec(lines[0].replaceAll(" ", ""))[0])
+const distance = parseInt(/\d+/g.exec(lines[1].replaceAll(" ", ""))[0])
+
+const ends = getEnds(time, distance)
+const tolerance = ends.upper - ends.lower + 1
+
+function getEnds(time, distance) {
+    const fraction = time / 2;
+    const t1 = fraction - Math.sqrt(fraction**2 - (distance + 1))
+    const t2 = fraction + Math.sqrt(fraction**2 - (distance + 1))
+
+    return {
+        lower: Math.ceil(t1),
+        upper: Math.floor(t2)
+    }
+}
+
+console.log(`The amount of possible ways to win are: ${tolerance}`)
+
+const BENCHMARK_END = process.hrtime.bigint()
+console.log(`Execution time: ${(BENCHMARK_END - BENCHMARK_START) / 1000000n}ms`)
