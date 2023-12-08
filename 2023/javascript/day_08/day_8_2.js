322a41633ff398abeaf5b76c871c5f2e5476eb8b
ZeldaFan0225 <78901316+ZeldaFan0225@users.noreply.github.com>
2023-12-08 17:11:56 +0100

    complete day 8 - 2

--- 2023/javascript/day_08/day_8_2.js
+++ 2023/javascript/day_08/day_8_2.js
@@ -4,7 +4,7 @@ const BENCHMARK_START = process.hrtime.bigint()
 const input = readFileSync("./input.txt", "utf-8").replace(/\r/g, "")
 const [instruction, nodes_text] = input.split("\n\n")
 const nodes_lines = nodes_text.split("\n")
-const nodes_regex = /^\(([A-Z]{3}), ([A-Z]{3})\)$/
+const nodes_regex = /^\(([A-Z0-9]{3}), ([A-Z0-9]{3})\)$/
 
 const instructions = instruction.split("").map(i => i === "L" ? 0 : 1)
 const nodes = {}
@@ -22,17 +22,33 @@ for(let node of Object.keys(nodes)) {
     if(node.endsWith(("A"))) curr_nodes.push(node)
 }
 
-let i = 0;
-while(curr_nodes.some(node => !node.endsWith("Z"))) {
-    for(let ind = 0; ind < curr_nodes.length; ind++) {
-        if(!curr_nodes[ind].endsWith("Z")) {
-            curr_nodes[ind] = nodes[curr_nodes[ind]][instructions[i % instructions.length]]
-        }
-    }
-    i++
+const lengths = []
+
+for(let node of curr_nodes) {
+    lengths.push(chainLength(node))
 }
 
-console.log(`Benötigte Schritte: ${i}`)
+function chainLength(start) {
+    let ind = 0;
+    let current_node = start;
+    while(!current_node.endsWith("Z")) {
+        current_node = nodes[current_node][instructions[ind % instructions.length]]
+        ind++
+    }
+
+    return ind;
+}
+
+function gcd (a, b) {
+    if(b === 0) return a;
+    return gcd(b, a%b);
+}
+
+function lcm (a, b) {
+    return Math.abs(a * b) / gcd(a, b)
+}
+
+console.log(`Benötigte Schritte: ${lengths.reduce(lcm)}`)
 
 const BENCHMARK_END = process.hrtime.bigint()
 console.log(`Execution time: ${(BENCHMARK_END - BENCHMARK_START) / 1000n}µs`)
